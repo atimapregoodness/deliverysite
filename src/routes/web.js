09ff5015d59bcc3cab0edc8456a15cc5314cf08c
Atimapre Goodness <atimaxgoodness@gmail.com>
2026-01-09 09:57:58 +0100

    updating pages

--- src/routes/web.js
+++ src/routes/web.js
@@ -17,4 +17,69 @@ router.get("/track", (req, res) => {
   });
 });
 
+// Security routes
+router.get("/security/static-guards", (req, res) => {
+  res.render("pages/security/static-guards", { currentPage: "security" });
+});
+
+router.get("/security/mobile-patrols", (req, res) => {
+  res.render("pages/security/mobile-patrols", { currentPage: "security" });
+});
+
+router.get("/security/alarm-monitoring", (req, res) => {
+  res.render("pages/security/alarm-monitoring", { currentPage: "security" });
+});
+
+router.get("/security/cctv", (req, res) => {
+  res.render("pages/security/cctv", { currentPage: "security" });
+});
+
+router.get("/security/cyber", (req, res) => {
+  res.render("pages/security/cyber", { currentPage: "security" });
+});
+
+// Logistics routes
+router.get("/logistics/freight-forwarding", (req, res) => {
+  res.render("pages/logistics/freight-forwarding", {
+    currentPage: "logistics",
+  });
+});
+
+router.get("/logistics/warehousing", (req, res) => {
+  res.render("pages/logistics/warehousing", { currentPage: "logistics" });
+});
+
+router.get("/logistics/distribution", (req, res) => {
+  res.render("pages/logistics/distribution", { currentPage: "logistics" });
+});
+
+router.get("/logistics/cold-chain", (req, res) => {
+  res.render("pages/logistics/cold-chain", { currentPage: "logistics" });
+});
+
+router.get("/logistics/supply-chain", (req, res) => {
+  res.render("pages/logistics/supply-chain", { currentPage: "logistics" });
+});
+
+// Company routes
+router.get("/about", (req, res) => {
+  res.render("pages/company/about", { currentPage: "company" });
+});
+
+router.get("/contact", (req, res) => {
+  res.render("pages/company/contact", { currentPage: "company" });
+});
+
+router.get("/company/leadership", (req, res) => {
+  res.render("pages/company/leadership", { currentPage: "company" });
+});
+
+router.get("/company/careers", (req, res) => {
+  res.render("pages/company/careers", { currentPage: "company" });
+});
+
+router.get("/company/certifications", (req, res) => {
+  res.render("pages/company/certifications", { currentPage: "company" });
+});
+
 module.exports = router;
